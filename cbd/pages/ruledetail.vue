a3d33e2efc81276cabef1907a698067f67dbbdb1
erdan1996 <604691708@qq.com>
2019-02-22 15:13:32 +0800

    rules-detail

---
+++ cbd/pages/ruledetail.vue
@@ -0,0 +1,71 @@
+<!--  -->
+<template>
+  <div class="rulede">
+    <div class="w">
+      <div class="rd-top">
+        <h4>
+          国家税务总局公告2018年第28号 国家税务总局关于发布
+        </h4>
+        <div class="rd-top-line">
+          <span>
+            发表日期：2018-10-29
+          </span>
+          <span>
+            来源：国家税务总局甘肃省税务局
+          </span>
+          <span>
+            作者：国家税务总局甘肃省税务局
+          </span>
+        </div>
+      </div>
+      <div class="rd-bo">
+        <p>
+          国家税务总局公告2018年第28号
+        </p>
+        <p>       
+          国家税务总局关于发布《企业所得税税前扣除凭证管理办法》的公告
+        </p>
+        <p>       
+          为加强企业所得税税前扣除凭证管理，规范税收执法，优化营商环境，国家税务总局制定了《企业所得税税前扣除凭证管理办法》，现予以发布。特此公告现予以发布。特
+          此公告。。
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  meta: {
+    title: '智慧库'
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang='less' scoped>
+.rd-top {
+  text-align: center;
+  border-bottom: 1px solid #e1e2e6;
+  padding-top: 20px;
+  padding-bottom: 10px;
+  h4 {
+    font-size: 24px;
+    color: #282d38;
+    font-weight: 400;
+    text-align: center;
+    margin-bottom: 10px;
+  }
+  span {
+    color: #747d8c;
+    margin-right: 15px;
+  }
+}
+.rd-bo {
+  box-sizing: border-box;
+  padding: 150px 0;
+}
+.rulede {
+  background-color: #fff;
+}
+</style>
